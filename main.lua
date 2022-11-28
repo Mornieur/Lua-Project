b8ebed2b69f459878baeda97c009d00d28e6d730
Mornieur <mariafernanda-coro@hotmail.com>
2022-11-27 22:50:59 -0300

    feat: add ball require function

--- main.lua
+++ main.lua
@@ -1,13 +1,24 @@
 require("player")
+require("ball")
 
 function love.load()
   Player:load()
+  Ball:load()
 end 
 
 function love.update(dt)
   Player:update(dt)
+  Ball:update(dt)
 end
 
 function love.draw()
   Player:draw()
+  Ball:draw()
+end
+
+function checkCollision(a, b)
+  if a.x + a.width > b.x and a.x < b.x + b.width and a.y + a.height > b.y and a.y < b.y + b.height then
+    return true
+  else 
+    return false
 end
