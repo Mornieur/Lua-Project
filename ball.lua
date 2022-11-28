b3f26030d19bbb6f3333085ed12b95a8a9f62784
Mornieur <mariafernanda-coro@hotmail.com>
2022-11-27 22:50:40 -0300

    feat: add ball move, draw and collision

---
+++ ball.lua
@@ -0,0 +1,43 @@
+Ball = {}
+
+function Ball:load()
+  self.x = love.graphics.getWidth() / 2
+  self.y = love.graphics.getHeight() / 2
+  self.width = 20
+  self.height = 20
+  self.speed = 200
+  self.xVel = -self.speed
+  self.yVel = 0
+end
+
+function Ball:update(dt)
+  self:move(dt)
+  self:collide()
+end
+
+function Ball:collide()
+  if checkCollision(self, player) then
+    self.xVel = self.speed
+    local middleBall = self.y + self.height /2
+    local middlePlayer = Player.y + Player.height / 2
+    local collisionPosition = middleBall - middlePlayer
+    self.yVel = collisionPosition * 5
+  end
+
+  if self.y < 0 then 
+    self.y = 0
+    self.yVel = -self.yVel
+  elseif self.y + self.height > love.graphics.getHeight() then 
+    self.y = love.graphics.getHeight() - self.height
+    self.yVel = -self.yVel
+  end
+end
+
+function Ball:move(dt)
+  self.x = self.xVel * dt
+  self.y = self.yVel * dt
+end
+
+function Ball:draw()
+  love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+end
